4a05b569374fbbba699a90b25514099d56da85dd
yellowsubmarine372 <yellowsubmarine372@gmail.com>
2025-06-30 10:45:00 +0900

    lession4-5 commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View, StatusBar } from 'react-native';
 import { Stack } from 'expo-router';
+import { Colors } from '../constants/Colors';
 
 const RootLayout = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+
     return (
-        <View style={{ flex: 1 }}>
+        <>
+            <StatusBar value="auto" />
             <Stack screenOptions={{ 
-                headerStyle: { backgroundColor: '#ddd' },
-                headerTintColor: '#333',
+                headerStyle: { backgroundColor: theme.navBackground },
+                headerTintColor: theme.title,
             }}>
                 <Stack.Screen name="index" options={{ title: 'Home' }} />
                 <Stack.Screen name="about" options={{ title: 'About' }} />
                 <Stack.Screen name="contact" options={{ title: 'Contact' }} />
             </Stack>
             <Text>Footer</Text>
-        </View>
+        </>
     )
 }
 
--- app/about.jsx
+++ app/about.jsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { Link } from 'expo-router';
+import { Colors } from '../constants/Colors';
 
 const About = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>About Page</Text>
+        <View style={[styles.container, { backgroundColor: theme.background }]}>
+            <Text style={[styles.title, { color: theme.title }]}>About Page</Text>
 
             <Link href="/" style={styles.link}>Back Home</Link>
         </View>
