0efc637c47f87dc9f22bfe158dcd5e28789f0786
yellowsubmarine372 <yellowsubmarine372@gmail.com>
2025-07-11 14:59:40 +0900

    lesson18 commit

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -7,13 +7,13 @@ import ThemedView from "../../components/ThemedView"
 import ThemedButton from '../../components/ThemedButton'
 
 const Profile = () => {
-  const { logout } = useUser()
+  const { logout, user } = useUser()
 
   return (
     <ThemedView style={styles.container}>
 
       <ThemedText title={true} style={styles.heading}>
-        Your Email
+        {user.email}
       </ThemedText>
       <Spacer />
 
--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { account } from '../lib/appwrite'
 import { ID } from 'appwrite'
 
@@ -6,6 +6,7 @@ export const UserContext = createContext()
 
 export function UserProvider({ children }) {
     const [user, setUser] = useState(null)
+    const [authChecked, setAuthChecked] = useState(false)
 
     /*evoke function to login*/
     async function login(email, password) {
@@ -33,8 +34,23 @@ export function UserProvider({ children }) {
         setUser(null)
     }
 
+    async function getInitialUserValue() {
+        try {
+            const response = await account.get()
+            setUser(response)
+        } catch (error) {
+            setUser(null)
+        } finally {
+            setAuthChecked(true)
+        }
+    }
+
+    useEffect(() => {
+        getInitialUserValue()
+    }, [])
+
     return (
-        <UserContext.Provider value={{ user, login, register, logout}}>
+        <UserContext.Provider value={{ user, login, register, logout, authChecked}}>
             {children}
         </UserContext.Provider>
     )
